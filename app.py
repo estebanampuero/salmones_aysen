96e8c75ba94ebe5ef0306c4178ea510672cc4012
estebanampuero <31929548+estebanampuero@users.noreply.github.com>
2025-08-09 17:13:16 +0000

    cambios finales

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from docxtpl import DocxTemplate
@@ -11,19 +11,24 @@ class DataModel(BaseModel):
     titulo: str
     items: list[str]
 
+
 @app.post("/generate-docx/")
-async def generate_docx(data: DataModel):
+async def generate_docx(data: DataModel, background_tasks: BackgroundTasks):
     template_path = "base.docx"
     doc = DocxTemplate(template_path)
     doc.render(data.dict())
     output_filename = f"output_{uuid.uuid4()}.docx"
     doc.save(output_filename)
 
-    # Retorna el archivo generado y luego lo elimina después de enviarlo
-    response = FileResponse(
+    def remove_file(path):
+        try:
+            os.remove(path)
+        except Exception:
+            pass
+
+    background_tasks.add_task(remove_file, output_filename)
+    return FileResponse(
         output_filename,
         media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        filename=output_filename,
-        background=None  # Puedes usar BackgroundTask para eliminar el archivo después
+        filename=output_filename
     )
-    return response
